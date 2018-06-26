6e80eb6db26a0112d7944b0e3c8edc5dd82423ca
Andrew Carlson <acarl005@g.ucla.edu>
2018-06-25 22:38:43 -0700

    figure out scraping method

---
+++ scrape.sh
@@ -0,0 +1,6 @@
+# first get the missing person IDs from these CSVs:
+# https://drive.google.com/open?id=1pQ6Q9HYnDo-aDfPannJIh2IN2pBkQmyR
+# https://drive.google.com/open?id=1JQl0huUyhft1MJ62af4mvit1V3wd4SsX
+# then make this request:
+curl 'https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/50759' > '50759.json'
+# this downloads a JSON response which contains A URL to their thumbnail and full photo
