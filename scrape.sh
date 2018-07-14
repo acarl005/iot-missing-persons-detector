d7d5fe1ba6f21a72ab080bf39eb02d7aea5ef66d
Andrew Carlson <acarl005@g.ucla.edu>
2018-07-13 20:55:39 -0700

    add jq command

--- scrape.sh
+++ scrape.sh
@@ -2,5 +2,6 @@
 # https://drive.google.com/open?id=1pQ6Q9HYnDo-aDfPannJIh2IN2pBkQmyR
 # https://drive.google.com/open?id=1JQl0huUyhft1MJ62af4mvit1V3wd4SsX
 # then make this request:
-curl 'https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/50759' > '50759.json'
+curl 'https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/50435' > '50435.json'
 # this downloads a JSON response which contains A URL to their thumbnail and full photo
+jq '.images[].files.original.href' '50435.json'
